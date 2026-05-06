f5950dffcd67d5dc521e0ab8a870cec7f0862229
Fuzja <fuzjajadrowa2137@gmail.com>
2026-05-06 19:28:52 +0200

    final build for every target with kotlin custom postprocessor

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -1,43 +1,10 @@
-import org.gradle.kotlin.dsl.replace
-
 plugins {
     id("dev.kikugie.stonecutter")
 }
-stonecutter active "1.21.4-neoforge"
+stonecutter active "26.1-fabric"
 
 stonecutter parameters {
     swaps["mod_version"] = "\"" + property("mod.version") + "\";"
     swaps["minecraft"] = "\"" + node.metadata.version + "\";"
     constants["release"] = property("mod.id") != "template"
-
-    replacements {
-        string {
-            direction = eval(current.version, ">=1.21.11")
-            replace("ResourceLocation", "Identifier")
-        }
-        string {
-            direction = eval(current.version, "<1.21.11")
-            replace("Identifier", "ResourceLocation")
-        }
-        string {
-            direction = eval(current.version, "<1.21.11")
-            replace("import net.minecraft.world.entity.player.PlayerSkin;", "import net.minecraft.client.resources.PlayerSkin;")
-        }
-        string {
-            direction = eval(current.version, "<1.21.11")
-            replace(".identifier()", ".location()")
-        }
-        string {
-            direction = eval(current.version, "<26.1")
-            replace("GuiGraphicsExtractor", "GuiGraphics")
-        }
-        string {
-            direction = eval(current.version, "<26.1")
-            replace(".text(", ".drawString(")
-        }
-        string {
-            direction = eval(current.version, "<26.1")
-            replace(".centeredText(", ".drawCenteredString(")
-        }
-    }
 }
