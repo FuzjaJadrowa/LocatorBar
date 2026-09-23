7e92925c3ad97456b0c7104fc464bbb993826d92
Fuzja <fuzjajadrowa2137@gmail.com>
2026-09-23 19:55:50 +0200

    deleting unnessesary tests

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,20 +10,4 @@ repositories {
 
 dependencies {
     implementation("me.modmuss50:mod-publish-plugin:1.1.0")
-    testImplementation(kotlin("test-junit"))
-    testImplementation("com.google.code.gson:gson:2.11.0")
-}
-
-sourceSets.test {
-    java.srcDir("../src/common/src/main/java")
-    java.include("pl/fuzjajadrowa/locatorbar/config/**", "pl/fuzjajadrowa/locatorbar/util/MarkerMath.java")
-}
-
-tasks.test {
-    useJUnit()
-    systemProperty("locatorbar.root", projectDir.parentFile.absolutePath)
-    workingDir = layout.buildDirectory.dir("test-work").get().asFile
-    doFirst { workingDir.mkdirs() }
-    inputs.dir("../src")
-    inputs.file("../settings.gradle.kts")
 }
